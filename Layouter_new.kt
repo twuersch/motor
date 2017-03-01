d890ce6161c318819bf3ff6b8876e50b7bfa91ee
Timo Würsch <twuersch@gmail.com>
2017-03-01 08:10:24 +0100

    More layouter restructuring.

--- Layouter_new.kt
+++ Layouter_new.kt
@@ -1,4 +1,5 @@
 import org.jsoup.nodes.Node
+import org.jsoup.nodes.TextNode
 
 /**
  * Created by timo on 24.02.17.
@@ -9,6 +10,7 @@ object Layouter_new {
     if (Layouter.hasBlockTile(node)) {
       layoutBlockNode(node, parentBlock)
     } else if (Layouter.hasTextTile(node)) {
+      // TODO: Explicit cast?
       layoutTextNode(node, parentBlock)
     } else if (Layouter.isFormatElement(node)) {
       layoutFormatElement(node, parentBlock)
@@ -19,21 +21,16 @@ object Layouter_new {
     /*
     Idea: Grow from inside out: Set current block tile as parent's y,
     and after layouting, add to the parent's height.
-
-    - Create block tile:
-      (- set padding to 0 for document, html etc.)
-      - width = parent with - parent padding (✓)
-      - height = my top padding + my bottom padding (✓)
-      - x = parent x + parent left padding (✓)
-      - y = parent height - parent bottom padding + parent y (✓)  ((1) margin between blocks either here, as y += margin...)
-    - Destroy text wrapper tile if present (✓)
-    - Attach myself as parent's child (✓)
-    - Lay out children nodes with this block tile as their parent (✓)
-    - Measure content height by measuring the newly laid out children (✓)
-    - Set my content height (✓)
-    - Grow parent content height by my own height ((1)....or here) (✓)
      */
 
+    /*
+    Create block tile:
+    (- set padding to 0 for document, html etc.)
+    - width = parent with - parent padding
+    - height = my top padding + my bottom padding
+    - x = parent x + parent left padding
+    - y = parent height - parent bottom padding + parent y ((TODO) (1) margin between blocks either here, as y += margin...)
+     */
     val blockTile = BlockTile(
       node = node,
       width = parentBlock.width - parentBlock.leftPadding - parentBlock.rightPadding,
@@ -41,31 +38,53 @@ object Layouter_new {
       y = parentBlock.y + parentBlock.height - parentBlock.bottomPadding
     )
 
+    // Destroy text wrapper tile if present
     if (TextLayoutState.wrapperBlockTile != null) {
       TextLayoutState.wrapperBlockTile = null
     }
 
+    // Attach myself as parent's child
     parentBlock.children.add(blockTile)
 
+    // Lay out children nodes with this block tile as their parent
     for (childNode in node.childNodes()) {
       layout(childNode, blockTile)
     }
 
+    /*
+    - Measure content height by measuring the newly laid out children
+    - Set my content height
+    - Grow parent content height by my own height ((1)....or here)
+     */
     val childrenTilesHeight = measureHeight(blockTile.children)
     blockTile.contentHeight(childrenTilesHeight)
     parentBlock.growContentHeight(blockTile.contentHeight())
   }
 
-  fun layoutTextNode(node: Node, parentBlock: AnonymousBlockTile) {
+  fun layoutTextNode(node: TextNode, parentBlock: AnonymousBlockTile) {
     /*
-    - Create wrapper tile if not present: (✓)
-      (- Set my padding to 0.)
-      - width = parent with - parent padding (✓)
-      - height = top padding + bottom  padding, effectively 0 at the start (✓)
-      - y = parent height - parent bottom padding + parent y (✓)
-      - Attach myself as parent's child. (✓)
-    - position word(s). Grow wrapper tile height and parent block height when positioning the first word of a new line
+    - Create wrapper tile if not present:
+      (- Set my padding to 0.) (TODO)
+      - width = parent with - parent padding
+      - height = top padding + bottom  padding, effectively 0 at the beginning
+      - y = parent height - parent bottom padding + parent y
+      - Attach myself as parent's child.
      */
+    if (TextLayoutState.wrapperBlockTile == null) {
+      val wrapperBlockTile = AnonymousBlockTile(
+        width = parentBlock.width - parentBlock.leftPadding - parentBlock.rightPadding,
+        y = parentBlock.y + parentBlock.height - parentBlock.bottomPadding,
+      )
+      parentBlock.children.add(wrapperBlockTile)
+    }
+
+    /*
+     Iterate over words to position them.
+     Note that an Iterator is required here to be able to modify the list of words
+     while iterating.
+     */
+    val wordsIterator = node.text().split(Regex("\\s+")).toMutableList().listIterator()
+
   }
 
   fun layoutFormatElement(node: Node, parentBlock: AnonymousBlockTile) {
@@ -78,46 +97,7 @@ object Layouter_new {
    * Captures the state for text layout
    */
   object TextLayoutState {
-    var cursorX: Int = 0 // relative to parent block tile
-    var cursorY: Int = 0 // relative to parent block tile
-    var remainingWidthPx: Int = 0
-    var bold: Int = 0 // Counter (not simple flag) to guard against bad nesting
-    var italic: Int = 0
     var wrapperBlockTile: AnonymousBlockTile? = null
-
-    fun resetCursor(lineWidthPx: Int) {
-      cursorX = 0
-      remainingWidthPx = lineWidthPx
-    }
-
-    fun moveCursorRightBy(px: Int) {
-      cursorX += px
-      remainingWidthPx -= px
-    }
-
-    fun setBold() {
-      bold = bold + 1
-    }
-
-    fun unsetBold() {
-      bold = Math.max(0, bold - 1)
-    }
-
-    fun isBold(): Boolean {
-      return (bold > 0)
-    }
-
-    fun setItalic() {
-      italic = italic + 1
-    }
-
-    fun unsetItalic() {
-      italic = Math.max(0, italic - 1)
-    }
-
-    fun isItalic(): Boolean {
-      return (italic > 0)
-    }
   }
 
   data class Interval<out T>(val min: T, val max: T)
